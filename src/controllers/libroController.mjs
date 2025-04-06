cea65ba6d7712ab5f54fd6227f3d9c182ddd290b
Esteban <davidballesterosc2903@gmail.com>
2025-04-05 23:08:09 -0500

    Se realizo el cambio a los dos metodos existentes del controlador del libro, ajustandolos a los nuevos espacion de la base de datos

--- src/controllers/libroController.mjs
+++ src/controllers/libroController.mjs
@@ -5,7 +5,7 @@ import pool from "../database/connection.mjs";
 //Consultar todo
 export const getAllLibro = async (req, res) => {
     try {
-      const [rows] = await pool.query('SELECT * FROM Libro');
+      const [rows] = await pool.query('SELECT * FROM libro');
       res.json(rows);
     } catch (error) {
       res.status(500).json({ error: error.message });
@@ -15,16 +15,18 @@ export const getAllLibro = async (req, res) => {
 
 // Crear un nuevo
 export const createLibro = async (req, res) => {
-    const { nombre, autor, editorial, año, ubicación, edicion, genero, descripcion, condicion} = req.body;
+    const { nombre, año, ubicacion, edicion, descripcion, condicion, id_editorial, id_genero} = req.body;
     try {
       const result = await pool.query(
-        'INSERT INTO libro (nombre, autor, editorial, año, ubicación, edicion, genero, descripcion, condicion) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
-        [nombre, autor, editorial, año, ubicación, edicion, genero, descripcion, condicion]
+        'INSERT INTO libro (nombre, año, ubicacion, edicion, descripcion, condicion, id_editorial, id_genero) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+        [nombre, año, ubicacion, edicion, descripcion, condicion, id_editorial, id_genero]
       );
       res.status(201).json({ id: result[0].insertId });
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
   };
+
+
 
   
